1ce60d5e00a97465fd92c7543a78e2ee23ab8d0f
JC Harrington <jc@brandibble.co>
2020-05-03 12:04:36 -0500

    Persist state to localStorage every 3 second susing lodash throttle function

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import throttle from 'lodash/throttle'
+import { loadState, saveState } from '../utils/localStorage'
 import counterReducer from '../features/counter/counterSlice'
 import configReducer from '../slices/configSlice'
 import orderReducer from '../slices/orderSlice'
 import locationsReducer from '../slices/locationsSlice'
 import menuReducer from '../slices/menuSlice'
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-    config: configReducer,
-    order: orderReducer,
-    locations: locationsReducer,
-    menu: menuReducer,
-  },
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  config: configReducer,
+  order: orderReducer,
+  locations: locationsReducer,
+  menu: menuReducer,
 })
+
+const persistedState = loadState()
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: persistedState,
+})
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState())
+  }, 3000)
+)
+
+export default store
