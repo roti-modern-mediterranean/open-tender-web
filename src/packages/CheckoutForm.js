4990e8d82d4508fafbdc807ef00632b01e4960db
JC Harrington <jc@brandibble.co>
2020-05-20 22:07:30 -0500

    Handle automatic adjustment of tenders as discounts, etc. are adjusted

--- src/packages/CheckoutForm.js
+++ src/packages/CheckoutForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, createContext } from 'react'
+import React, { useState, useRef, createContext, useEffect } from 'react'
 import propTypes from 'prop-types'
 import CheckoutDetails from './CheckoutDetails'
 import CheckoutCustomer from './CheckoutCustomer'
@@ -10,6 +10,44 @@ import CheckoutTenders from './CheckoutTenders'
 import { checkAmountRemaining } from './utils'
 
 export const FormContext = createContext(null)
+
+const adjustTenders = (tenders, isPaid, amountRemaining, updateForm) => {
+  if (!tenders.length || isPaid) return
+  const gift = tenders.filter((i) => i.tender_type === 'GIFT_CARD')
+  const nonGift = tenders.filter((i) => i.tender_type !== 'GIFT_CARD')
+  if (amountRemaining > 0) {
+    if (!nonGift.length) return
+    const newTenders = nonGift.map((i) => {
+      const newAmount = parseFloat(i.amount) + amountRemaining
+      amountRemaining = 0.0
+      return { ...i, amount: newAmount.toFixed(2) }
+    })
+    updateForm({ tenders: [...gift, ...newTenders] })
+    isPaid = Math.abs(amountRemaining).toFixed(2) === '0.00'
+  } else {
+    const newTenders = nonGift.map((i) => {
+      const newAmount = Math.max(parseFloat(i.amount) + amountRemaining, 0.0)
+      amountRemaining += parseFloat(i.amount) - newAmount
+      return { ...i, amount: newAmount.toFixed(2) }
+    })
+    isPaid = Math.abs(amountRemaining).toFixed(2) === '0.00'
+    if (isPaid) {
+      const nonZero = newTenders.filter((i) => i.amount !== '0.00')
+      updateForm({ tenders: [...gift, ...nonZero] })
+    } else {
+      const newGift = gift.map((i) => {
+        const newAmount = Math.max(parseFloat(i.amount) + amountRemaining, 0.0)
+        amountRemaining += parseFloat(i.amount) - newAmount
+        return { ...i, amount: newAmount.toFixed(2) }
+      })
+      const allNew = [...newGift, ...newTenders].filter(
+        (i) => i.amount !== '0.00'
+      )
+      updateForm({ tenders: allNew })
+      isPaid = Math.abs(amountRemaining).toFixed(2) === '0.00'
+    }
+  }
+}
 
 const CheckoutForm = ({
   config,
@@ -24,21 +62,25 @@ const CheckoutForm = ({
 }) => {
   const [isWorking, setIsWorking] = useState(false)
   const submitButton = useRef()
+  const { total } = check ? check.totals : 0.0
+  const { tenders } = form
+  let amountRemaining = checkAmountRemaining(total, tenders)
+  let isPaid = Math.abs(amountRemaining).toFixed(2) === '0.00'
+
+  useEffect(() => {
+    adjustTenders(tenders, isPaid, amountRemaining, updateForm)
+  }, [tenders, isPaid, amountRemaining, updateForm])
+
   if (!check || !check.config) return null
+
+  const isDelivery = order.serviceType === 'DELIVERY'
+  const hasGiftCardTender = check.config.tender_types.includes('GIFT_CARD')
 
   const handleSubmit = (evt) => {
     evt.preventDefault()
     setIsWorking(true)
     submitButton.current.blur()
   }
-
-  const isDelivery = order.serviceType === 'DELIVERY'
-  const hasGiftCardTender = check.config.tender_types.includes('GIFT_CARD')
-  const amountRemaining = checkAmountRemaining(check.totals.total, form.tenders)
-  const isPaid = Math.abs(amountRemaining).toFixed(2) === '0.00'
-  // const isBalance = amountRemaining !== 0
-  // const showGiftCards = hasGiftCardTender && (isBalance || form.giftCards)
-  // const showTenders = isBalance || form.tenders
 
   return (
     <FormContext.Provider
@@ -68,6 +110,18 @@ const CheckoutForm = ({
         {hasGiftCardTender && <CheckoutGiftCards />}
         <CheckoutTenders />
         <div className="form__footer">
+          <div className="form__message">
+            {!isPaid ? (
+              <p className="ot-alert-color">
+                There is a balance of ${amountRemaining.toFixed(2)} remaining on
+                your order. Please add a payment above.
+              </p>
+            ) : (
+              <p className="ot-success-color">
+                Your order can now be submitted. Go for it!
+              </p>
+            )}
+          </div>
           <input
             className="btn btn--big"
             type="submit"
