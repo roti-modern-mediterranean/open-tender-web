4255f8814d8de6af73cb9539d34e622d21268246
JC Harrington <jc@brandibble.co>
2020-07-08 18:58:08 -0500

    Initial work on mobile adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,40 +5,42 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className="footer ot-dark">
-      <div className="footer__container container flex">
-        <div className="footer__logo">
-          <span className="ot-font-size-small">Powered by</span>
-          <img src={logo} className="logo" alt="logo" />
+      <div className="container">
+        <div className="footer__container">
+          <div className="footer__logo">
+            <span className="ot-font-size-small">Powered by</span>
+            <img src={logo} className="logo" alt="logo" />
+          </div>
+          <nav className="footer__nav">
+            <ul>
+              <li>
+                <a
+                  className="no-link ot-link-light"
+                  href="https://demo.brandibble.co/order/terms/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Terms of Use
+                </a>
+              </li>
+              <li>
+                <a
+                  className="no-link ot-link-light"
+                  href="https://demo.brandibble.co/order/privacy/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Pricvacy Policy
+                </a>
+              </li>
+              <li>
+                <Link to="/refunds" className="no-link ot-link-light">
+                  Refunds
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="footer__nav">
-          <ul>
-            <li>
-              <a
-                className="no-link ot-link-light"
-                href="https://demo.brandibble.co/order/terms/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Terms of Use
-              </a>
-            </li>
-            <li>
-              <a
-                className="no-link ot-link-light"
-                href="https://demo.brandibble.co/order/privacy/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Pricvacy Policy
-              </a>
-            </li>
-            <li>
-              <Link to="/refunds" className="no-link ot-link-light">
-                Refunds
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </footer>
   )
