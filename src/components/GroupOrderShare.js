245c5ca2ca224dbf21cad8a35ca2002123120daf
JC Harrington <jc@brandibble.co>
2020-08-12 18:02:00 -0500

    Remove unused

--- src/components/GroupOrderShare.js
+++ src/components/GroupOrderShare.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
 import {
   selectGroupOrder,
   resetGroupOrder,
