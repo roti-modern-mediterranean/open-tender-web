8fa406f38c18695cbbb4242f05b696feb4fb5a7e
JC Harrington <jc@brandibble.co>
2020-08-14 19:44:11 -0500

    Attempt react-helmet implementation

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ import ErrorBoundary from './components/ErrorBoundary'
 import { fetchConfig } from './slices/configSlice'
 import './App.scss'
 
+// const gtmScript = (gtmId) => {
+//   const cleanId = gtmId.replace('GTM-', '')
+//   return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-${cleanId}');`
+// }
+
 class App extends React.Component {
   componentDidMount() {
     this.props.fetchConfig()
@@ -41,11 +50,11 @@ class App extends React.Component {
               <meta name="description" content={brand.description} />
               <link rel="icon" href={brand.favicon} />
               <link rel="apple-touch-icon" href={brand.appleTouchIcon} />
+              {/* <script type="application/ld+json">{gtmScript('MLR3VV8')}</script> */}
               <link href={body.url} rel="stylesheet" />
               {headings.url && body.url !== headings.url && (
                 <link href={headings.url} rel="stylesheet" />
               )}
-              <link href="" rel="stylesheet" />
             </Helmet>
             <Router>
               <Modal />
