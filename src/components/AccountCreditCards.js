8a07893f8ccc6b4adb5e1398bc7a75bf60d57f8c
JC Harrington <jc@brandibble.co>
2020-11-30 10:30:26 -0600

    Minor change to account credit cards

--- src/components/AccountCreditCards.js
+++ src/components/AccountCreditCards.js
@@ -51,14 +51,14 @@ const AccountCreditCards = () => {
           </SectionHeader>
           <SectionLoading loading={isLoading} />
           <SectionError error={error} />
-          {showCreditCards && (
+          {showCreditCards ? (
             <div className="section__content -max ot-bg-color-primary ot-border ot-border-radius">
               <CreditCards
                 creditCards={creditCards.entities}
                 isLoading={isLoading}
               />
             </div>
-          )}
+          ) : null}
         </div>
       </div>
     </div>
