3c545f3d7033e0e3a705fe1f92527d373502b850
JC Harrington <jc@brandibble.co>
2021-03-30 11:03:21 -0500

    Add Terms page placeholder

---
+++ src/components/pages/Terms/Terms.js
@@ -0,0 +1,91 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { isBrowser } from 'react-device-detect'
+import { useSelector } from 'react-redux'
+import { Helmet } from 'react-helmet'
+import { handleRespError } from '@open-tender/js'
+
+import { maybeRefreshVersion } from '../../../app/version'
+import { selectAPI, selectBrand } from '../../../slices'
+import { AppContext } from '../../../App'
+import {
+  Content,
+  Main,
+  PageTitle,
+  PageContainer,
+  PageContent,
+  HeaderDefault,
+} from '../..'
+
+const Terms = () => {
+  const { windowRef } = useContext(AppContext)
+  const [page, setPage] = useState(null)
+  const [error, setError] = useState(null)
+  const { title: siteTitle } = useSelector(selectBrand)
+  const api = useSelector(selectAPI)
+  const slug = 'terms'
+  const pageTitle = page ? page.title : 'Not Found'
+
+  useEffect(() => {
+    windowRef.current.scrollTop = 0
+    maybeRefreshVersion()
+  }, [windowRef])
+
+  useEffect(() => {
+    let isCancelled = false
+    const fetchPage = async () => {
+      try {
+        let page = await api.getPage(slug)
+        if (!isCancelled) {
+          setPage(page)
+        }
+      } catch (err) {
+        if (!isCancelled) {
+          let { detail } = handleRespError(err)
+          if (detail.includes('does not exist')) {
+            detail = 'Page not found. Please try again.'
+          }
+          setError(detail)
+        }
+      }
+    }
+    if (slug) fetchPage()
+    return () => {
+      isCancelled = true
+    }
+  }, [api, slug])
+
+  return (
+    <>
+      <Helmet>
+        <title>
+          {pageTitle} | {siteTitle}
+        </title>
+      </Helmet>
+      <Content>
+        <HeaderDefault title={isBrowser ? null : pageTitle} />
+        <Main>
+          <PageContainer style={{ maxWidth: '76.8rem' }}>
+            {page ? (
+              <>
+                <PageTitle title={page.title} subtitle={page.subtitle} />
+                <PageContent style={{ textAlign: 'left', marginTop: '3rem' }}>
+                  <p>Page content goes here</p>
+                </PageContent>
+              </>
+            ) : error ? (
+              <>
+                <PageTitle title="Something went wrong" />
+                <PageContent style={{ textAlign: 'left', marginTop: '3rem' }}>
+                  <p>{error}</p>
+                </PageContent>
+              </>
+            ) : null}
+          </PageContainer>
+        </Main>
+      </Content>
+    </>
+  )
+}
+
+Terms.displayName = 'Terms'
+export default Terms
