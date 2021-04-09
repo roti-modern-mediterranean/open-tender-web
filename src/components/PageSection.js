8bd2660d8b0a211e34837049510f7152ede9411f
JC Harrington <jc@brandibble.co>
2021-04-09 15:28:40 -0500

    Small updates

--- src/components/PageSection.js
+++ src/components/PageSection.js
@@ -18,7 +18,7 @@ const PageSectionFooter = styled('div')`
   margin: 1rem 0 0;
   @media (max-width: ${(props) => props.theme.breakpoints.tablet}) {
     max-width: 40rem;
-    margin: -1rem auto 0;
+    margin: 0 auto;
   }
 `
 
