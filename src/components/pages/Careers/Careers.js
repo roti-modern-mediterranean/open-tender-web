0d2551e35efc762e3121e85a946651993417af6d
JC Harrington <jc@brandibble.co>
2021-04-23 09:23:14 -0500

    Fix Careers page import bug

--- src/components/pages/Careers/Careers.js
+++ src/components/pages/Careers/Careers.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, useCallback } from 'react'
 import { isBrowser } from 'react-device-detect'
 import { useSelector } from 'react-redux'
 import { Helmet } from 'react-helmet'
@@ -17,16 +17,15 @@ import {
   PageContent,
   HeaderDefault,
 } from '../..'
-import { useCallback } from 'react/cjs/react.development'
 import styled from '@emotion/styled'
 
 const OneMoreThingWrapper = styled('div')`
-  background-color: #C6D0DD;
-  h3{
+  background-color: #c6d0dd;
+  h3 {
     font-weight: normal;
     font-size: 2em;
     margin: 0.5rem 0 0.5em 0;
-    color: ${(props) => props.theme.colors.pepper };
+    color: ${(props) => props.theme.colors.pepper};
     font-family: ${(props) => props.theme.fonts.preface.family};
   }
   > div {
@@ -49,7 +48,7 @@ const BenefitsWrapper = styled('ul')`
   flex-wrap: wrap;
   @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
   }
-  li{
+  li {
     width: calc(25% - 1em);
     margin: 1em 0.5em 0 0;
     text-align: center;
@@ -60,19 +59,19 @@ const BenefitsWrapper = styled('ul')`
       flex-direction: column;
     }
   }
-  h4{
+  h4 {
     font-weight: normal;
     font-size: 1em;
     text-align: left;
     margin: 0.75rem 0 0 1em;
-    color: ${(props) => props.theme.colors.pepper };
+    color: ${(props) => props.theme.colors.pepper};
     font-family: ${(props) => props.theme.fonts.preface.family};
     @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
       font-size: 1.25em;
       text-align: center;
     }
   }
-  img{
+  img {
     width: 3.75em;
     display: inline-block;
   }
@@ -90,7 +89,7 @@ const FrameImage = styled('div')`
 const ContentListOfItems = styled('ul')`
   display: flex;
   @media (max-width: ${(props) => props.theme.breakpoints.narrow}) {
-    background: transparent
+    background: transparent;
   }
   @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
     flex-direction: column;
@@ -102,7 +101,7 @@ const ContentListOfItems = styled('ul')`
   li {
     flex: 1;
     margin-right: 1em;
-    h3{
+    h3 {
       font-weight: normal;
       font-size: 1.5em;
       margin: 0.75rem 0;
@@ -111,7 +110,7 @@ const ContentListOfItems = styled('ul')`
       }};
       font-family: ${(props) => props.theme.fonts.preface.family};
     }
-    p{
+    p {
       font-size: 0.85em;
       @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
         font-size: 1em;
@@ -130,7 +129,7 @@ const ContentCards = styled('ul')`
   margin: 4em 0;
 
   @media (max-width: ${(props) => props.theme.breakpoints.narrow}) {
-    background: transparent
+    background: transparent;
   }
   @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
     flex-direction: column;
@@ -142,7 +141,7 @@ const ContentCards = styled('ul')`
   li {
     flex: 1 1 auto;
     margin-right: 1em;
-    h3{
+    h3 {
       font-weight: normal;
       font-size: 1.5em;
       margin: 0.75rem 0;
@@ -151,7 +150,7 @@ const ContentCards = styled('ul')`
       }};
       font-family: ${(props) => props.theme.fonts.preface.family};
     }
-    p{
+    p {
       font-size: 0.85em;
       @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
         font-size: 1em;
@@ -167,9 +166,7 @@ const ContentCards = styled('ul')`
   }
 `
 
-
 const Careers = () => {
-  
   const { windowRef } = useContext(AppContext)
   const [page, setPage] = useState(null)
   const [error, setError] = useState(null)
@@ -179,38 +176,41 @@ const Careers = () => {
   const pageTitle = page ? page.title : 'Not Found'
 
   const createAnnouncement = useCallback(() => {
-    return {
-      settings: {
-        announcement_page_id: Math.floor(Math.random() * 18000) + 1,
-        autoplay: false,
-        duration: 3000,
-        duration_mobile: 2500,
-        page: 'Careers',
-        show_arrows: false,
-        show_arrows_mobile: false,
-        show_dots: false,
-        show_dots_mobile: false,
-        transition: 1000,
-        transition_mobile: 500
-      }, entities: [
-        {
-          announcement_id: Math.floor(Math.random() * 18000) + 1,
-          display_order: 1,
-          horizontal: 'CENTER',
-          images: page.files,
-          background_size: 'contain',
-          slide_color: '#B7C9D7',
-          overlay_color: "25272A",
-          overlay_opacity: 0.5,
-          show_overlay: true,
-          text_color: "FBF8EA",
-          title: "JOIN THE ROTEAM",
-          url: 'https://google.com',
-          url_text: 'Apply today',
-          vertical: "BOTTOM"
+    return page
+      ? {
+          settings: {
+            announcement_page_id: Math.floor(Math.random() * 18000) + 1,
+            autoplay: false,
+            duration: 3000,
+            duration_mobile: 2500,
+            page: 'Careers',
+            show_arrows: false,
+            show_arrows_mobile: false,
+            show_dots: false,
+            show_dots_mobile: false,
+            transition: 1000,
+            transition_mobile: 500,
+          },
+          entities: [
+            {
+              announcement_id: Math.floor(Math.random() * 18000) + 1,
+              display_order: 1,
+              horizontal: 'CENTER',
+              images: page.files,
+              background_size: 'contain',
+              slide_color: '#B7C9D7',
+              overlay_color: '25272A',
+              overlay_opacity: 0.5,
+              show_overlay: true,
+              text_color: 'FBF8EA',
+              title: 'JOIN THE ROTEAM',
+              url: 'https://google.com',
+              url_text: 'Apply today',
+              vertical: 'BOTTOM',
+            },
+          ],
         }
-      ]
-    }
+      : {}
   }, [page])
 
   useEffect(() => {
@@ -252,15 +252,11 @@ const Careers = () => {
       <Content>
         <HeaderDefault title={isBrowser ? null : pageTitle} />
         <Main>
-          {
-            page &&
-              <PageView>
-                <PageHero
-                  announcements={createAnnouncement()}
-                  showHero={true}
-                />
-              </PageView>
-          }
+          {page && (
+            <PageView>
+              <PageHero announcements={createAnnouncement()} showHero={true} />
+            </PageView>
+          )}
           <PageContainer style={{ maxWidth: '86.8rem', margin: '0rem auto' }}>
             {page ? (
               <>
@@ -268,34 +264,77 @@ const Careers = () => {
                 <PageContent style={{ textAlign: 'left', marginTop: '3rem' }}>
                   <ContentListOfItems>
                     <li>
-                      <img alt='' src='https://roti.com/carousels/careers/small-image-careers-top-1.jpg' />
+                      <img
+                        alt=""
+                        src="https://roti.com/carousels/careers/small-image-careers-top-1.jpg"
+                      />
                       <h3>Joy.</h3>
-                      <p>Rōti brings people together: over a meal, during a shift, and across the country in our many locations. Creating and being a part of these meaningful connections gives us joy, and we’re committed to paying that joy forward to every single person, every single day. We know that joy - a smile, a laugh, or thoughtful question - can turn any day around.</p>
+                      <p>
+                        Rōti brings people together: over a meal, during a
+                        shift, and across the country in our many locations.
+                        Creating and being a part of these meaningful
+                        connections gives us joy, and we’re committed to paying
+                        that joy forward to every single person, every single
+                        day. We know that joy - a smile, a laugh, or thoughtful
+                        question - can turn any day around.
+                      </p>
                     </li>
                     <li>
-                      <img alt='' src='https://roti.com/carousels/careers/small-image-careers-top-2.jpg' />
+                      <img
+                        alt=""
+                        src="https://roti.com/carousels/careers/small-image-careers-top-2.jpg"
+                      />
                       <h3>Pride.</h3>
-                      <p>Our vibe is our superpower - we take pride in creating an environment that focuses on hospitality in fun and engaging ways. No task is too small to devote attention to and to get right. This makes a difference in the lives of our customers and our community, but also in the lives of our team members.</p>
+                      <p>
+                        Our vibe is our superpower - we take pride in creating
+                        an environment that focuses on hospitality in fun and
+                        engaging ways. No task is too small to devote attention
+                        to and to get right. This makes a difference in the
+                        lives of our customers and our community, but also in
+                        the lives of our team members.
+                      </p>
                     </li>
                     <li>
-                      <img alt='' src='https://roti.com/carousels/careers/small-image-careers-top-3.png' />
+                      <img
+                        alt=""
+                        src="https://roti.com/carousels/careers/small-image-careers-top-3.png"
+                      />
                       <h3>Progress.</h3>
-                      <p>We’re always looking for ways to improve ourselves and our business - to get better, go farther, and grow faster with every day. What does that mean for you? That you’ll be on the fast track in a fast-paced environment. We are big believers that big wins start from small beginnings - so get your start towards a big future.</p>
+                      <p>
+                        We’re always looking for ways to improve ourselves and
+                        our business - to get better, go farther, and grow
+                        faster with every day. What does that mean for you? That
+                        you’ll be on the fast track in a fast-paced environment.
+                        We are big believers that big wins start from small
+                        beginnings - so get your start towards a big future.
+                      </p>
                     </li>
                   </ContentListOfItems>
                   <ContentCards>
                     <li>
-                      <img alt='' src='https://roti.com/images/image-c-1.png' />
-                      <div className='card-body'>
+                      <img alt="" src="https://roti.com/images/image-c-1.png" />
+                      <div className="card-body">
                         <h3>A Career that matters.</h3>
-                        <p>At Rōti it’s simple - we hire people with positive energy, who accomplish goals by supporting each other and are committed to serving Food That Matters. Whether you start as a Team Member, Manager, or part of our Support Team, we will empower your personal and professional development.</p>
+                        <p>
+                          At Rōti it’s simple - we hire people with positive
+                          energy, who accomplish goals by supporting each other
+                          and are committed to serving Food That Matters.
+                          Whether you start as a Team Member, Manager, or part
+                          of our Support Team, we will empower your personal and
+                          professional development.
+                        </p>
                       </div>
                     </li>
                     <li>
-                      <img alt='' src='https://roti.com/images/image-c-2.png' />
-                      <div className='card-body'>
-                        <h3>Inclusion,  diversity &amp; equity.</h3>
-                        <p>We support and encourage a diverse workforce. We’re looking for people to be themselves, whatever that entails. After all, you’re at your best when you can bring your whole self to work.</p>
+                      <img alt="" src="https://roti.com/images/image-c-2.png" />
+                      <div className="card-body">
+                        <h3>Inclusion, diversity &amp; equity.</h3>
+                        <p>
+                          We support and encourage a diverse workforce. We’re
+                          looking for people to be themselves, whatever that
+                          entails. After all, you’re at your best when you can
+                          bring your whole self to work.
+                        </p>
                       </div>
                     </li>
                   </ContentCards>
@@ -303,35 +342,35 @@ const Careers = () => {
                     <h3>Benefits</h3>
                     <BenefitsWrapper>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-medical.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-medical.png" />
                         <h4>Medical, dental, vision</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-bonus.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-bonus.png" />
                         <h4>Bonus opportunities</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-tip-pool.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-tip-pool.png" />
                         <h4>Tip pool program</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-flexible.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-flexible.png" />
                         <h4>Flexible schedules</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-career.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-career.png" />
                         <h4>career &amp; education opportunities</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-timeoff.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-timeoff.png" />
                         <h4>Paid time off</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-401k.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-401k.png" />
                         <h4>401k retirement savings</h4>
                       </li>
                       <li>
-                        <FrameImage src='https://roti.com/images/icons/benefits-freefood.png' />
+                        <FrameImage src="https://roti.com/images/icons/benefits-freefood.png" />
                         <h4>discounts &amp; free food</h4>
                       </li>
                     </BenefitsWrapper>
@@ -349,13 +388,28 @@ const Careers = () => {
           </PageContainer>
           <OneMoreThingWrapper>
             <PageContainer style={{ maxWidth: '86.8rem', margin: '3rem auto' }}>
-              <div className='one-wrapper'>
+              <div className="one-wrapper">
                 <h3>Hello, 2021. We’re hiring.</h3>
-                <p>We’ve got big plans for this year. Opening restaurants across the country, updating and innovating our menus, and always, always thinking of ways to bring our food to more people. Think you could help us? We hope so. Check out our open roles to see if anything is a match. </p>
+                <p>
+                  We’ve got big plans for this year. Opening restaurants across
+                  the country, updating and innovating our menus, and always,
+                  always thinking of ways to bring our food to more people.
+                  Think you could help us? We hope so. Check out our open roles
+                  to see if anything is a match.{' '}
+                </p>
               </div>
-              <ButtonStyled onClick={() => {
-                window.open('https://recruiting.ultipro.com/ROT1002/JobBoard/4fc826aa-4426-6136-502d-c0778bee0567/?q=&o=postedDateDesc&w=&wc=&we=&wpst=', '_blank');
-              }} size="big" color="secondary">Explore roles</ButtonStyled>
+              <ButtonStyled
+                onClick={() => {
+                  window.open(
+                    'https://recruiting.ultipro.com/ROT1002/JobBoard/4fc826aa-4426-6136-502d-c0778bee0567/?q=&o=postedDateDesc&w=&wc=&we=&wpst=',
+                    '_blank'
+                  )
+                }}
+                size="big"
+                color="secondary"
+              >
+                Explore roles
+              </ButtonStyled>
             </PageContainer>
           </OneMoreThingWrapper>
         </Main>
